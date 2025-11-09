4d70f573a89f47a2ef8cc3c21b41161ed21f8269
donskikh <donskihpavel8@gmail.com>
2025-11-10 00:51:13 +0300

    added handler and payload struct

---
+++ internal/auth/handler.go
@@ -0,0 +1,41 @@
+package auth
+
+import (
+	"fmt"
+	"go/adv-demo/configs"
+	"go/adv-demo/pkg/res"
+	"net/http"
+)
+
+type AuthHandlerDeps struct {
+	*configs.Config
+}
+
+type AuthHandler struct {
+	*configs.Config
+}
+
+func NewAuthHandler(router *http.ServeMux, deps AuthHandlerDeps) {
+	handler := &AuthHandler{
+		Config: deps.Config,
+	}
+	router.HandleFunc("POST /auth/login", handler.Login())
+	router.HandleFunc("POST /auth/register", handler.Register())
+}
+
+func (handler *AuthHandler) Login() http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		fmt.Println(handler.Config.Auth.Secret)
+		fmt.Println("Login")
+		data := LoginResponse{
+			Token: "123",
+		}
+		res.Json(w, data, 200)
+	}
+}
+
+func (handler *AuthHandler) Register() http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		fmt.Println("Register")
+	}
+}
