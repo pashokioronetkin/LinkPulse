fa10ffbd9a25f7f46d58d35e11d81de4d4bc5411
Pavel Donskikh <donskihpavel8@gmail.com>
2026-01-23 21:07:45 +0300

    context + parse method from pkg/jwt/jwt.go

--- pkg/middleware/auth.go
+++ pkg/middleware/auth.go
@@ -1,16 +1,41 @@
 package middleware
 
 import (
-	"fmt"
+	"context"
+	"go/adv-demo/configs"
+	"go/adv-demo/pkg/jwt"
 	"net/http"
 	"strings"
 )
 
-func IsAuthed(next http.Handler) http.Handler {
+type key string
+
+const (
+	ContextEmailKey key = "ContextEmailKey"
+)
+
+func wtiteUnauthed(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusUnauthorized)
+	w.Write([]byte(http.StatusText(http.StatusUnauthorized)))
+}
+
+func IsAuthed(next http.Handler, config *configs.Config) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		authedHeader := r.Header.Get("Authorization")
+		if !strings.HasPrefix(authedHeader, "Bearer ") {
+			wtiteUnauthed(w)
+			return
+		}
 		token := strings.TrimPrefix(authedHeader, "Bearer ")
-		fmt.Println(token)
-		next.ServeHTTP(w, r)
+		isValid, data := jwt.NewJWT(config.Auth.Secret).Parse(token)
+		if !isValid {
+			wtiteUnauthed(w)
+			return
+		}
+		r.Context()
+		ctx := context.WithValue(r.Context(), ContextEmailKey, data.Email)
+		req := r.WithContext(ctx) // Update request with context
+
+		next.ServeHTTP(w, req)
 	})
 }
